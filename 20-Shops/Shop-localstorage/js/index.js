d68f835c07348624767f75e6c877b367348e05c8
Igor-lab-git <igorkost142@gmail.com>
2025-12-09 21:41:22 +0300

    fix-22

---
+++ 20-Shops/Shop-localstorage/js/index.js
@@ -0,0 +1,102 @@
+"use strict"
+//==========================================
+import { 
+    showErrorMessage,
+    setBasketLocalStorage,
+    getBasketLocalStorage,
+    checkingRelevanceValueBasket
+} from './utils.js';
+
+import { 
+    COUNT_SHOW_CARDS_CLICK, 
+    ERROR_SERVER,
+    NO_PRODUCTS_IN_THIS_CATEGORY
+} from './constants.js';
+
+const cards = document.querySelector('.cards');
+const btnShowCards = document.querySelector('.show-cards');
+let shownCards = COUNT_SHOW_CARDS_CLICK;
+let countClickBtnShowCards = 1;
+let productsData = [];
+
+getProducts();
+
+async function getProducts() {
+    try {
+        if(!productsData.length) { // что бы лишний раз не делать запрос на сервер, проверяем существующий массив и его длинну продуктов если он не пустой не функю не вызывается
+            const res = await fetch("../data/products.json");
+            if(!res.ok) {
+                throw new Error(res.statusText);
+            }
+            productsData = await res.json(); 
+
+            if((productsData.length > COUNT_SHOW_CARDS_CLICK) && btnShowCards.classList.contains("none")) { //если товара в массиве больше 5 то показываем кнопку показать ещё
+                btnShowCards.classList.remove("none");
+            }
+            console.log(productsData);
+            
+            renderStartPage(productsData);
+        }
+    } catch (error) {
+        showErrorMessage(ERROR_SERVER);
+        console.log(error.message);
+        
+    }
+};
+
+function renderStartPage(data) {
+    if(!data || !data.length) { // ещё раз проверяем наличие данных в массиве перед началом функции 
+        showErrorMessage(NO_PRODUCTS_IN_THIS_CATEGORY);
+        return; // и обязательно return что-бы функцияя дальше не выполнялась
+    };
+
+    const arrCards = data.slice(0, COUNT_SHOW_CARDS_CLICK); // режим нашь массив что-бы ренлдерить порциями до 5
+    createCards(arrCards)
+    
+
+};
+
+function createCards(arrCards) {
+
+}
+//Интернет-магазин на JavaScript. Кнопка показать еще. Карточка товара. Корзина. Local Storage.20-00
+
+
+
+
+
+
+
+// Рендер карточки
+function createCards(data) {
+    data.forEach(card => {
+        const { id, img, title, price, discount } = card;
+        const priceDiscount = price - ((price * discount) / 100);
+		const cardItem = 
+			`
+                <div class="card" data-product-id="${id}">
+                    <div class="card__top">
+                        <a href="/card.html?id=${id}" class="card__image">
+                            <img
+                                src="./images/${img}"
+                                alt="${title}"
+                            />
+                        </a>
+                        <div class="card__label">-${discount}%</div>
+                    </div>
+                    <div class="card__bottom">
+                        <div class="card__prices">
+                            <div class="card__price card__price--discount">${priceDiscount}</div>
+                            <div class="card__price card__price--common">${price}</div>
+                        </div>
+                        <a href="/card.html?id=${id}" class="card__title">${title}</a>
+                        <button class="card__add">В корзину</button>
+                    </div>
+                </div>
+            `
+        cards.insertAdjacentHTML('beforeend', cardItem);
+	});
+}
+
+
+
