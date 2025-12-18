d6168b501e88ee8284edbfed1e3f262686c26470
Igor-lab-git <igorkost142@gmail.com>
2025-12-18 21:45:18 +0300

    fix-35

--- 22-todo-list/02-Todo-List-and-LocalStorage/js/main.js
+++ 22-todo-list/02-Todo-List-and-LocalStorage/js/main.js
@@ -1,39 +1,55 @@
+
+//1-10
 const form = document.querySelector("#form");
 const inputElement = document.querySelector("#taskInput");
 const tasksListElement = document.querySelector("#tasksList");
 const emptyListElement = document.querySelector("#emptyList");
 const confirmModal = document.querySelector("#confirmModal");
 
+let dataTasks = [];
+
 form.addEventListener("submit", (e) => addTask(e));
 
 inputElement.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     addTask(e);
-  }
+  };
 });
 tasksListElement.addEventListener("click", (e) => findButtonsTaskItem(e));
 
+
+
 function addTask(e) {
   e.preventDefault();
-  const taskText = inputElement.value.trim().toLowerCase();
+  const taskText = inputElement.value.trim();
 
-  if (taskText !== "") {
-    tasksListElement.insertAdjacentHTML("beforeend", insertTaskHTML(taskText));
-  }
+  const newTask = {
+    id: Date.now() + Math.floor(Math.random() * 1000),
+    text: taskText,
+    done: false,
+  };
+  dataTasks.push(newTask);
 
+  if (dataTasks.length !== 0) {
+    tasksListElement.insertAdjacentHTML("beforeend", insertTaskHTML(dataTasks));
+
+  };
+  console.log(dataTasks)
   inputElement.value = "";
   inputElement.focus();
   if (tasksListElement.children.length > 1) {
     emptyListElement.classList.add("none");
-  }
-}
+  };
 
-function insertTaskHTML(taskText) {
+};
+
+function insertTaskHTML(arrayTasks) {
   let taskHTML = "";
-  if (taskText) {
-    taskHTML = `
-        <li class="list-group-item d-flex justify-content-between task-item">
-			<span class="task-title">${taskText}</span>
+  arrayTasks.forEach((task) => {
+    if (task) {
+      taskHTML = `
+        <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+			<span class="task-title" >${task.text}</span>
 			<div class="task-item__buttons">
 				<button type="button" data-action="done" class="btn-action">
 					<img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -43,10 +59,10 @@ function insertTaskHTML(taskText) {
 				</button>
 			</div>
 		</li>
-    `;
-  }
+    `;};
+  });
   return taskHTML;
-}
+};
 
 function findButtonsTaskItem(e) {
   const button = e.target.closest("[data-action]");
@@ -77,13 +93,25 @@ function deleteTask(taskItem, taskTitle) {
     } else if (e.target.closest("#cancelBtn")) {
       confirmModal.classList.remove("active");
     };
-    
   });
 };
 
-
+//========================== Переключение выполненных таксок
 function doneTask(taskItem, taskTitle) {
   if (taskItem) {
     taskTitle.classList.toggle("task-title--done");
   };
 };
+
+
+//========================== local-Storage
+
+function setTaskLS(key, item) {
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+function getTaskLS(key) {
+  const taskListHTML = localStorage.getItem(key);
+  return taskListHTML ?  JSON.parse(taskListHTML) : "";
+};
+
