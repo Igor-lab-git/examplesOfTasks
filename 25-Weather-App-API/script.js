becb6bde2dc110bb7be4adfc2c84002f6bcc924d
Igor-lab-git <igorkost142@gmail.com>
2025-12-26 20:32:46 +0300

    fix-39

--- 25-Weather-App-API/script.js
+++ 25-Weather-App-API/script.js
@@ -1,71 +1,41 @@
-const apiKey = 'Api-key';
-const card = document.querySelector('.card');
-const searchBox = document.querySelector('.card__input');
-const searchBtn = document.getElementById('searchBtn');
-const temp = document.querySelector('.card__title--temp');
-const cityName = document.getElementById('city');
-const humidity = document.getElementById('humidity');
-const wind = document.getElementById('wind');
-const icon = document.querySelector('.card__weather-icon');
-const date = document.getElementById('date');
 
+const accessKey = "961f0e9d-f48c-4c12-8227-41961701db74";
+const cityName = 'USA'; // Город можно брать из поля ввода
+const headers = {
+  "X-Yandex-Weather-Key": accessKey,
+};
 
-const showDate = () => {
-  const now = new Date();
-  const day = now.getDate();
-  const monthName = now.toLocaleString('en', {month:'long'});
-  date.innerHTML = `${day} ${monthName}`;
+async function getCoordinates(cityName) {
+  const geoUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json`;
+  const response = await fetch(geoUrl);
+  const data = await response.json();
+
+  if (data && data[0]) {
+    return {
+      lat: data[0].lat,
+      lon: data[0].lon
+    };
+  } else {
+    throw new Error('Город не найден');
+  }
 }
 
-async function getWeather(city) {
-
-  city = city.trim();
-  if (!city) return;
-
+async function getWeather() {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const coords = await getCoordinates(cityName);
+    const url = `https://api.weather.yandex.ru/v2/forecast?lat=${coords.lat}&lon=${coords.lon}&extra=true`;
+    const response = await fetch(
+        url,
+      { headers }
+    );
 
-    if (!response.ok) throw new Error('City not found');
-
-    const result = await response.json();
-
-    console.log(result);
-
-    card.classList.add('active');
-    card.style.height = '530px';
-
-    const iconUrl = `https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`;
-    icon.src = iconUrl;
-
-    cityName.innerHTML = result.name;
-    humidity.innerHTML = `${result.main.humidity} %`;
-    wind.innerHTML = `${result.wind.speed} m/s`;
-    temp.innerHTML = `${Math.round(result.main.temp)}°C`;
-
-  } catch (err) {
-    alert(err);
-  }
-
-}
-
-searchBtn.addEventListener('click',() => {
-  getWeather(searchBox.value);
-});
-
-searchBox.addEventListener('keypress',(e) => {
-  if (e.key === 'Enter') {
-    getWeather(searchBox.value);
-  }
-});
-
-document.addEventListener('click',(e) => {
-  if (card.classList.contains('active') && !card.contains(e.target)) {
-    card.classList.remove('active');
-    card.style.height = '230px';
-    searchBox.value = '';
-  }
-})
-
-
-
-showDate();
+    if(!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  };
+};
+getWeather()
