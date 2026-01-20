1581bb42cfef2a793f97187d358398ff501bb275
Igor <igorkost142@gmail.com>
2026-01-20 16:20:40 +0300

    fix-50

---
+++ 29-reacts-practice/02-TYPE-SCRIPT/src/index.ts
@@ -0,0 +1,97 @@
+
+interface IUser {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+    isLove: boolean;
+    func: (user: IUser) => IUser;
+    success: string | number;
+    address: IAddress[];
+}
+
+interface IAddress {
+    country: string;
+    city: string;
+}
+
+const logName = (user: IUser): IUser => {
+    return user;
+}
+
+const user: IUser = {
+    id: 1,
+    name: "Jenna",
+    age: 23,
+    email: "jenna@yandex.ru",
+    isLove: true,
+    func: logName,
+    success: 200,
+    address: [
+        {
+            country: "USA",
+            city: "Los Angeles",
+        },
+        {
+            country: "USA",
+            city: "Los Angeles",
+        },
+        {
+            country: "USA",
+            city: "Los Angeles",
+        }
+    ]
+};
+
+console.log(user);
+console.log(logName(user));
+
+// ПРИМИТИВНЫЕ ТИПЫ
+// String
+// Number
+// BigInt
+// Boolean
+// undefined
+// null
+// Symbol
+
+// СПЕЦИАЛЬНЫЕ ТИПЫ
+// any отключает проверку типов
+// unknown
+// never
+// void
+// + литералы
+
+// union 
+let data: string | number;
+data = 15;
+data = "Jenna";
+
+type Color = "red" | "green" | "black";
+
+const color: Color ="green"
+
+const fruits: string[] = ["banan", "aple", "mango"];
+fruits.push("pear") //ok
+// fruits.push(5) //bad
+
+const things = ["Jenna", 5, true]; //автоматически определяет типы
+
+const addNumbers = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log(addNumbers(5, 15));
+
+
+//any
+let sumArg: any = null;
+sumArg = "Jenna";
+sumArg = 15;
+sumArg = true;
+
+let sumArr: any[] = ["Jenna", 5, true];
+console.log(sumArr.push(null));
+
+
+
