c77e55f170fe3fa0e5fd950aff731558b1fd4498
Igor-lab-git <igorkost142@gmail.com>
2026-02-11 22:30:51 +0300

    fix-movie-11

--- 29-reacts-practice/07-MOVIE-API-BULKOEV/Movie-API/src/pages/TopRankingsPage/TopRankingsPage.tsx
+++ 29-reacts-practice/07-MOVIE-API-BULKOEV/Movie-API/src/pages/TopRankingsPage/TopRankingsPage.tsx
@@ -1,28 +1,82 @@
-// import {useGetMoviesTopCollectionsQuery} from "../../app/store/ui/moviesApi.ts";
+import {useGetMoviesTopCollectionsQuery} from "../../app/store/ui/moviesApi.ts";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import {MOVIE_TOP_RANKINGS_LIST} from "../../shared/lib/constants.ts";
+import {useState} from "react";
+import ReactPaginate from "react-paginate";
+import style from "./TopRankingsPage.module.scss";
 
-interface IMovieListTop {
-  type: string;
-}
 
-const TopRankingsPage = ({type}: IMovieListTop) => {
+const TopRankingsPage = () => {
 
-    // const { data, error, isLoading } = useGetMoviesTopCollectionsQuery({type: "TOP_POPULAR_ALL", page: 1});
+    const params = useLocation();
+    const [numberPage, setNumberPage] = useState<number>(1);
+    const navigate = useNavigate();
+
+
+    const getTypeTopMovies = MOVIE_TOP_RANKINGS_LIST.find((item) => item.path === params.pathname);
+    // console.log(getTypeTopMovies);
+
+    const { data, error, isLoading } = useGetMoviesTopCollectionsQuery({type: getTypeTopMovies?.type, page: numberPage});
 
     // console.log(data, error, isLoading);
 
-    // if(isLoading) {
-    //     return <h2>Загрузка...</h2>
-    // };
+    const totalPages = data?.totalPages || 1;
 
-    // if(error) {
-    //     return <h2>Ошибка на сервере, данных о фильмах нет</h2>
-    // };
+    const handlePageClick = (event: { selected: number }) => {
+        setNumberPage(event.selected + 1); // React Paginate считает с 0
+    };
+
+    if(isLoading) return <h2>Загрузка...</h2>;
+    if(error) return <h2>Ошибка на сервере, данных о фильмах нет</h2>;
 
   return (
     <div>
-        <h1>{type === "TOP_POPULAR_MOVIES" ? "TOP_POPULAR_MOVIES" :  type === "TOP_250_MOVIES" ? "TOP_250_MOVIES" : ""}</h1>
+        <div>
+            <button onClick={() => navigate("/")}>на главную</button>
+            <button onClick={() => navigate(-1)}>назад</button>
+            <span>{getTypeTopMovies?.title}</span>
+        </div>
 
+        <div>
+            <ul>
+                {data && data.items.map((movie) => (
+                    <li key={movie.kinopoiskId}>
+                        <Link to={`/movie/${movie.kinopoiskId}`}>
+                            <img
+                                src={movie.posterUrlPreview}
+                                alt={movie.nameRu ? movie.nameRu : "постер фильма"}
+                                loading="lazy"
+                            />
+                            <span>{movie.nameRu ? movie.nameRu : movie.nameEn}</span>
+                            <span>IMDB {movie.ratingImdb ? movie.ratingImdb : "0"}</span>
+                            <span>Kinopoisk {movie.ratingKinopoisk ? movie.ratingKinopoisk : "0"}</span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
 
+        <div className={style.paginationWrapper}>
+            <ReactPaginate className={style.paginationList}
+                breakLabel="..."
+                nextLabel="Вперед →"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}    // сколько кнопок показывать
+                marginPagesDisplayed={2}  // сколько по краям
+                pageCount={totalPages}
+                previousLabel="← Назад"
+                renderOnZeroPageCount={null}
+                forcePage={numberPage - 1}
+
+                disabledClassName="pagination-disabled"
+                activeClassName={style.paginationActive}
+                containerClassName={style.pagination}
+                pageClassName={style.paginationPage}
+                previousClassName="pagination-prev"
+                nextClassName="pagination-next"
+                breakClassName="pagination-break"
+            />
+        </div>
     </div>
   )
 };
